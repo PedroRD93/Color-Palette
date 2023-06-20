85372d3008ec27e962c8f7cbe4047188078b96a0
Pedro Dias <Dias.PedroRibeiro@outlook.com>
2023-06-20 20:36:21 +0100

    Updates

---
+++ precache-manifest.f5255a1e115901651161f75133896306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d3bb995e6140af118b6",
+    "url": "/color-palette/static/css/main.9c67509e.chunk.css"
+  },
+  {
+    "revision": "8d3bb995e6140af118b6",
+    "url": "/color-palette/static/js/main.0ea89eab.chunk.js"
+  },
+  {
+    "revision": "5d67c3cd455a1d0948f4",
+    "url": "/color-palette/static/js/runtime~main.56200f79.js"
+  },
+  {
+    "revision": "b6f14c207a7e2d74ccd4",
+    "url": "/color-palette/static/css/2.df98f913.chunk.css"
+  },
+  {
+    "revision": "b6f14c207a7e2d74ccd4",
+    "url": "/color-palette/static/js/2.53e9045c.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/color-palette/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "234c77f0a034131263487f7c9726573d",
+    "url": "/color-palette/index.html"
+  }
+];
